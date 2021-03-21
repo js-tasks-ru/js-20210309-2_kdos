a86d2ce5ddad20a223101ab68e220b850db23a62
Andrey <kdos@bk.ru>
2021-03-21 13:30:44 +0300

    Объекты и массивы, основы тестирования

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,24 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  if (!path) {
+    return function (obj) {};
+  }
 
+  const pathFields = path.split(".");
+
+  return obj => {
+    if (!obj) {
+      return;
+    }
+
+    let fieldValue;
+    for (let i = 0; i < pathFields.length; i++) {
+      fieldValue = (i === 0 ? obj[pathFields[i]] : fieldValue[pathFields[i]]);
+      if (!fieldValue) {
+        return;
+      }
+    }
+    return fieldValue;
+  };
 }
